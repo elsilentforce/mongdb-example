fa84e7c1d9757fb5d12e6a6a56f42ec7c316f032
Patricio Tapia <native.devil@gmail.com>
2018-01-12 17:32:23 -0300

    ADD: db hook on test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/users_test', {
   useMongoClient: true
 });
+
 mongoose.connection
   .once('open', () => console.log('Good to go!'))
   .on('error', (error) => {
     console.warn('Warning', error);
   });
+
+beforeEach(() => {
+  mongoose.connection.collections.users.drop();
+});
