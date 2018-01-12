7b3bf449e1c7e7f11ea08c6614a29bf974ae69ce
Patricio Tapia <native.devil@gmail.com>
2018-01-12 17:15:09 -0300

    ADD: simple user on create_user

--- test/create_test.js
+++ test/create_test.js
@@ -1,5 +1,8 @@
+const assert = require('assert');
+const User = require('../src/user');
+
 describe('Creating records', () => {
   it('saves a user', () => {
-
+    const john = new User({ name: 'John' });
   });
 });
