e2a59b5d5a71788b08e3fe8f7b7aa68060323d3f
Patricio Tapia <native.devil@gmail.com>
2018-01-12 09:18:31 -0300

    FIX: test_helper.js

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
+mongoose.connect('mongodb://localhost/users_test', {
+  useMongoClient: true
+});
 mongoose.connection
   .once('open', () => console.log('Good to go!'))
   .on('error', (error) => {
