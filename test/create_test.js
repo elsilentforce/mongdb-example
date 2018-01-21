89416731d867798dd6bd8d9bd9c9909ca80ba695
Patricio Tapia <native.devil@gmail.com>
2018-01-21 18:39:12 -0300

    ADD: Assert on create_test

--- test/create_test.js
+++ test/create_test.js
@@ -2,8 +2,13 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
+  it('saves a user', (done) => {
     const john = new User({ name: 'John' });
-    john.save();
+    john.save()
+      .then(() = > {
+        // Has John has been saved successfully?
+        assert(!john.isNew);
+        done();
+      });
   });
 });
