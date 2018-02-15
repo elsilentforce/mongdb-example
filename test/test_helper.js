be73e458553841b814f56a1cb43adfc3db1b5d8d
Patricio Tapia <native.devil@gmail.com>
2018-02-15 07:53:39 -0300

    FIX: Mongoose promises

--- test/test_helper.js
+++ test/test_helper.js
@@ -4,11 +4,16 @@ mongoose.connect('mongodb://localhost/users_test', {
   useMongoClient: true
 });
 
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
+mongoose.Promise = global.Promise;
+
+before((done) => {
+  mongoose.connection
+  .once('open', () => { done(); } )
   .on('error', (error) => {
     console.warn('Warning', error);
   });
+});
+
 
 beforeEach(() => {
   mongoose.connection.collections.users.drop((done) => {
